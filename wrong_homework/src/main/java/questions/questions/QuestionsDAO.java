544e11da4d17215a7f8cfcaceb50af27166264a9
Matthew99-alt <137392578+Matthew99-alt@users.noreply.github.com>
2024-02-04 19:19:34 +0500

    Delete wrong_homework directory

--- wrong_homework/src/main/java/questions/questions/QuestionsDAO.java
+++
@@ -1,7 +0,0 @@
-package questions.questions;
-
-import java.util.ArrayList;
-
-public interface QuestionsDAO {
-    ArrayList<String> findQuestion();
-}
--- wrong_homework/src/main/java/questions/service/PersonService.java
+++
@@ -1,7 +0,0 @@
-package questions.service;
-
-import java.util.ArrayList;
-
-public interface PersonService {
-    void askUser(String name, ArrayList<String> userAnswers);
-}
